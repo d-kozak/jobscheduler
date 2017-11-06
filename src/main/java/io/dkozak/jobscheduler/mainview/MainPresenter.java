0132a581d4e119ccd91c38cf75e14622c3dbdace
dkozak <dkozak94@gmail.com>
2017-11-06 17:58:00 +0100

    remove configuration that was used just to prove that noise injection works

--- src/main/java/io/dkozak/jobscheduler/mainview/MainPresenter.java
+++ src/main/java/io/dkozak/jobscheduler/mainview/MainPresenter.java
@@ -5,25 +5,16 @@ import javafx.scene.text.Text;
 import lombok.extern.log4j.Log4j;
 
 import javax.annotation.PostConstruct;
-import javax.inject.Inject;
 
 @Log4j
 public class MainPresenter {
     @FXML
     public Text text;
 
-    @Inject
-    private String localMessage;
-
-    @Inject
-    private String globalMessage;
-
     @PostConstruct
     public void init() {
         log.info("init()");
 
-        log.info("global message: " + globalMessage);
-        log.info("local message: " + localMessage);
     }
 
 }
